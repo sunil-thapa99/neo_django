0e829bfa86eb485c39141bc531f3f32e6127cbca
lappanchappan43 <lappanchappan43@gmail.com>
2018-12-25 19:29:34 +0545

    Profile signal created in user_profile model

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+	# user = models.OneToOneField(User, on_delete=models.CASCADE)
+	# Bottom one is bad practice
+	user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+	# Now put all these as null
+	mobile = models.CharField(max_length=15, null=True, blank=True)
+	address = models.CharField(max_length=30, null=True, blank=True)
+
+	def __str__(self):
+		return '{} {}'.format(self.user.username, self.mobile)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+    
